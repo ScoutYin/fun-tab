777b32f5062abc37cd6922e338a31926a7adfdba
ScoutYin <scoutyin.1900@gmail.com>
2022-05-08 17:08:44 +0800

    chore: linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,19 @@ module.exports = {
 	env: {
 		node: true,
 		es2021: true,
-		'vue/setup-compiler-macros': true
+		'vue/setup-compiler-macros': true,
 	},
 	parser: 'vue-eslint-parser',
 	parserOptions: {
 		parser: '@typescript-eslint/parser',
 		ecmaVersion: 'latest',
-		sourceType: 'module'
+		sourceType: 'module',
 	},
 	plugins: ['@typescript-eslint', 'vue'],
 	extends: [
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:vue/vue3-recommended',
-		'plugin:prettier/recommended'
-	]
+		'plugin:prettier/recommended',
+	],
 };
