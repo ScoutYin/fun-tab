54afb86280dcee9b9cdfb89fecf7ffaa3f945f79
ScoutYin <scoutyin.1900@gmail.com>
2022-05-21 09:41:01 +0800

    build: copy files

--- build/gulpfile.ts
+++ build/gulpfile.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { series, parallel } from 'gulp';
 import { copy } from 'fs-extra';
 import { buildModules } from './modules';
@@ -5,9 +6,20 @@ import { generateTypesDefinitions } from './types-definitions';
 import { buildBundle } from './bundle';
 import { run } from './utils/process';
 import { buildConfig } from './config';
-import { typesDir } from './path';
+import { typesDir, projectRoot, pkgDir } from './path';
 
 const clean = () => run('rimraf dist');
+
+const copyFiles = async () => {
+	copy(
+		path.resolve(projectRoot, 'package.json'),
+		path.resolve(pkgDir, 'package.json')
+	);
+	copy(
+		path.resolve(projectRoot, 'README.md'),
+		path.resolve(pkgDir, 'README.md')
+	);
+};
 
 const copyTypesDefinitions = async () => {
 	return Promise.all(
@@ -20,7 +32,11 @@ const copyTypesDefinitions = async () => {
 export default series(
 	clean,
 	parallel(
+		copyFiles,
 		buildBundle,
-		series(parallel(buildModules, generateTypesDefinitions), copyTypesDefinitions)
+		series(
+			parallel(buildModules, generateTypesDefinitions),
+			copyTypesDefinitions
+		)
 	)
 );
